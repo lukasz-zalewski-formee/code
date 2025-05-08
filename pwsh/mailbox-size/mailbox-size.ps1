71b195336c76a691d60e96e0dd5b1067ab722422
Łukasz Zalewski <l.zalewski@formee.pl>
2025-05-08 08:48:14 +0200

    script for getting mailboxes size from exchange

---
+++ pwsh/mailbox-size/mailbox-size.ps1
@@ -0,0 +1,31 @@
+# Connect-ExchangeOnline 
+
+$output = @()
+
+$pattern = "\((.*?)\)"
+
+Get-Mailbox | Select-Object PrimarySmtpAddress | ForEach-Object {
+    $user = $_.PrimarySmtpAddress
+
+    $size = (Get-MailboxFolderStatistics $user | Where-Object {$_.FolderType -eq "Inbox"}).FolderSize
+
+    $size = [Regex]::Matches($size, $pattern).Value
+    $size = [long]($size -replace "bytes","" -replace "[\(*\)]","" -replace ",","" -replace " ", "")
+
+    $size /= 1GB
+
+    $output += [PSCustomObject]@{
+        User = $user
+        Size = "$size GB"
+    }
+}
+
+$output = $output | Sort-Object Size -Descending
+
+$output
+
+
+
+
+
+
